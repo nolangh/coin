0139ce3f6056f955451f55863b1b886d5cb96d6b
nolangh <nolanghiggins@gmail.com>
2022-06-07 22:39:57 -0600

    Added basic structure to value container

--- src/comps/Total.js
+++ src/comps/Total.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { AppBar, Window, WindowContent, WindowHeader } from "react95";
+import { Panel, Window, WindowContent, WindowHeader } from "react95";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
@@ -12,11 +12,18 @@ const TotalValue = () => {
 		<div className="Value">
 			<Wrapper>
 				<Window>
-					<WindowHeader>
-						<AppBar>
-							<h1>VALUE</h1>
-						</AppBar>
-					</WindowHeader>
+					<Panel
+						variant="outside"
+						shadow
+						style={{
+							padding: "0.5rem",
+							lineHeight: "1.5",
+							width: "100%",
+							height: "2em",
+						}}
+					>
+						<h1>VALUE</h1>
+					</Panel>
 					<WindowContent>
 						<h1>$2500</h1>
 						{/* NOTE UseState to change this when a new coin is added */}
