90992b211cb172346939f6144a0d15f9e4529472
nolangh <nolanghiggins@gmail.com>
2022-06-09 12:48:51 -0600

    added ract95 imports

--- src/comps/list.js
+++ src/comps/list.js
@@ -2,19 +2,25 @@ import { assets } from "./CoinInput";
 import styled from "styled-components";
 import { Window, Bar, WindowContent } from "react95";
 import React from "react";
+import "98.css";
 
 const Main = styled.div`
 	display: flex;
 	width: 50%;
 	height: 100%;
 	background-color: coral;
+
+	.bar {
+		width: 100%;
+		height: 3rem;
+	}
 `;
 
 const AssetList = () => {
 	return (
 		<Main>
 			<Window>
-				<Bar>ASSETS</Bar>
+				<Bar className="bar">ASSETS</Bar>
 				<WindowContent>
 					<h1>placeholder</h1>
 				</WindowContent>
