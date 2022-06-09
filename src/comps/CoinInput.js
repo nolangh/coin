339d68d7b0f46d2921193be852c7fbf34735cf4c
nolangh <nolanghiggins@gmail.com>
2022-06-09 11:23:02 -0600

    added a style library

--- src/comps/CoinInput.js
+++ src/comps/CoinInput.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Formik, Field, Form } from "formik";
+import "98.css";
 import {
 	Window,
 	WindowContent,
@@ -11,6 +12,7 @@ import {
 } from "react95";
 import styled from "styled-components";
 
+/* --------------------------------- Styles --------------------------------- */
 const CoinInput = styled.div`
 	width: 50%;
 	padding: 5rem;
@@ -19,18 +21,14 @@ const CoinInput = styled.div`
 		flex-direction: column;
 		background-color: green;
 	}
-	
+
 	.bar {
 		width: 100%;
 	}
-	.text {
-		width: 25%;
-		height: 1rem;
-	}
-	.number {
-		width: 10%;
-	}
 `;
+/* ------------------------------------------------------------------------ */
+
+const assets = [{}];
 
 const EnterCoin = () => (
 	<CoinInput>
@@ -48,26 +46,39 @@ const EnterCoin = () => (
 					onSubmit={async (values) => {
 						await new Promise((r) => setTimeout(r, 500));
 						// TODO - Change this to push the coin and amount to the main coin list
-						alert(JSON.stringify(values, null, 2));
+						assets.push(JSON.stringify(values, null, 2));
+						console.log(assets);
 					}}
 				>
 					<Form>
-						<label htmlFor="coin">COIN:</label>
-						<TextField
-							className="text"
-							id="coin"
-							name="coin"
-							placeholder="COIN"
-						/>
-						<Divider />
+						<div class="field-row">
+							<label htmlFor="coin" for="text17">
+								COIN:
+							</label>
+							<Field
+								className="text"
+								//id="coin"
+								id="text17"
+								type="text"
+								name="coin"
+								placeholder="COIN"
+							/>
+						</div>
 
-						<label htmlFor="amount">AMOUNT:</label>
-						<NumberField
-							className="number"
-							id="amount"
-							name="amount"
-							placeholder="0"
-						/>
+						<Divider />
+						<div class="field-row-stacked">
+							<label for="text24" htmlFor="amount">
+								AMOUNT:
+							</label>
+							<Field
+								className="number"
+								id="text24"
+								type="number"
+								value="0"
+								name="amount"
+								placeholder="0"
+							/>
+						</div>
 
 						<Button type="submit">ADD COIN</Button>
 					</Form>
