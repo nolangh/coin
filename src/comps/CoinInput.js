7b7bed9ffb114a4e144ccf6f3ecbfad979ea5cb9
nolangh <nolanghiggins@gmail.com>
2022-06-10 12:57:37 -0600

    added TotalValue to coin input comp

--- src/comps/CoinInput.js
+++ src/comps/CoinInput.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { Formik, Field, Form } from "formik";
 import "98.css";
 import { Window, WindowContent, Button, Bar, Divider } from "react95";
@@ -15,8 +15,25 @@ const CoinInput = styled.div`
 		flex-direction: column;
 		background-color: green;
 	}
+`;
 
+const Container = styled.div`
+	display: flex;
+	flex-direction: row;
+`;
+
+const Wrapper = styled.div`
+	padding: 5rem;
+	width: 50%;
+	background: ___CSS_0___;
+	.window {
+		text-align: center;
+		width: 100%;
+	}
 	.bar {
+		text-align: center;
+		height: 3rem;
+		font-size: 2rem;
 		width: 100%;
 	}
 `;
@@ -34,62 +51,71 @@ console.log(arraySum);
 
 const EnterCoin = () => {
 	return (
-		<CoinInput>
-			<Window className="window">
-				<Bar className="bar">ENTER COIN</Bar>
-				<WindowContent className="windowContent">
-					<Formik
-						className="form"
-						initialValues={{
-							coin: "",
-							amount: "",
-						}}
-						onSubmit={async (values) => {
-							await new Promise((r) => setTimeout(r, 500));
-							assets.push(JSON.stringify(values, null, 2));
-							TotalAssets.push(values.amount);
-							console.log(TotalAssets);
-						}}
-					>
-						<Form>
-							<div class="field-row">
-								<label htmlFor="coin" for="text17">
-									COIN:
-								</label>
-								<Field
-									className="text"
-									//id="coin"
-									id="text17"
-									type="text"
-									name="coin"
-									placeholder="COIN"
-								/>
-							</div>
+		<Container>
+			<CoinInput>
+				<Window className="window">
+					<Bar className="bar">ENTER COIN</Bar>
+					<WindowContent className="windowContent">
+						<Formik
+							className="form"
+							initialValues={{
+								coin: "",
+								amount: "",
+							}}
+							onSubmit={async (values) => {
+								await new Promise((r) => setTimeout(r, 500));
+								assets.push(JSON.stringify(values, null, 2));
+								TotalAssets.push(values.amount);
+								console.log(TotalAssets);
+							}}
+						>
+							<Form>
+								<div class="field-row">
+									<label htmlFor="coin" for="text17">
+										COIN:
+									</label>
+									<Field
+										className="text"
+										//id="coin"
+										id="text17"
+										type="text"
+										name="coin"
+										placeholder="COIN"
+									/>
+								</div>
 
-							<Divider />
+								<Divider />
 
-							<div class="field-row-stacked">
-								<label for="text24" htmlFor="amount">
-									AMOUNT:
-								</label>
-								<Field
-									className="number"
-									id="text24"
-									type="number"
-									name="amount"
-									placeholder="0"
-								/>
-							</div>
+								<div class="field-row-stacked">
+									<label for="text24" htmlFor="amount">
+										AMOUNT:
+									</label>
+									<Field
+										className="number"
+										id="text24"
+										type="number"
+										name="amount"
+										placeholder="0"
+									/>
+								</div>
 
-							<Button type="submit">ADD COIN</Button>
-						</Form>
-					</Formik>
-				</WindowContent>
-			</Window>
-		</CoinInput>
+								<Button type="submit">ADD COIN</Button>
+							</Form>
+						</Formik>
+					</WindowContent>
+				</Window>
+			</CoinInput>
+			<Wrapper>
+				<Window className="window">
+					<Bar className="bar">VALUE</Bar>
+
+					<WindowContent></WindowContent>
+				</Window>
+			</Wrapper>
+		</Container>
 	);
 };
 
 //ReactDOM.render(<Basic />, document.getElementById("root"));
 
-export { EnterCoin, arraySum };
+export default EnterCoin;
