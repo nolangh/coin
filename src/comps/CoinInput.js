4acb47cd57b0ee12d37a150c92f9d882531f3a7b
nolangh <nolanghiggins@gmail.com>
2022-06-11 17:30:34 -0600

    -a

---
+++ src/comps/CoinInput.js
@@ -0,0 +1,94 @@
+import React from "react";
+import { Formik, Field, Form } from "formik";
+import "98.css";
+import { Window, WindowContent, Button, Bar, Divider } from "react95";
+import styled from "styled-components";
+
+/* --------------------------------- Styles --------------------------------- */
+const CoinInput = styled.div`
+	width: 30%;
+	padding: 5rem;
+	background-color: lightyellow;
+	.form {
+		display: flex;
+		flex-direction: column;
+		background-color: green;
+	}
+
+	.bar {
+		width: 100%;
+	}
+`;
+
+/* ------------------------------------------------------------------------ */
+
+const TotalAssets = [];
+const assets = [{}];
+let sum = 0;
+
+const EnterCoin = () => {
+	function Add(v) {
+		sum += v;
+	}
+
+	return (
+		<CoinInput>
+			<Window className="window">
+				<Bar className="bar">ENTER COIN</Bar>
+				<WindowContent className="windowContent">
+					<Formik
+						className="form"
+						initialValues={{
+							coin: "",
+							amount: "",
+						}}
+						onSubmit={async (values) => {
+							await new Promise((r) => setTimeout(r, 500));
+							assets.push(JSON.stringify(values, null, 2));
+							TotalAssets.push(values.amount);
+							console.log(TotalAssets);
+							console.log(sum);
+							// useState to update TotalAssets when this is submited
+						}}
+					>
+						<Form>
+							<div class="field-row">
+								<label htmlFor="coin" for="text17">
+									COIN:
+								</label>
+								<Field
+									className="text"
+									//id="coin"
+									id="text17"
+									type="text"
+									name="coin"
+									placeholder="COIN"
+								/>
+							</div>
+
+							<Divider />
+							<div class="field-row-stacked">
+								<label for="text24" htmlFor="amount">
+									AMOUNT:
+								</label>
+								<Field
+									className="number"
+									id="text24"
+									type="number"
+									name="amount"
+									placeholder="0"
+								/>
+							</div>
+
+							<Button type="submit">ADD COIN</Button>
+						</Form>
+					</Formik>
+				</WindowContent>
+			</Window>
+		</CoinInput>
+	);
+};
+
+//ReactDOM.render(<Basic />, document.getElementById("root"));
+
+export { EnterCoin, assets };
---
+++ src/comps/List.js
@@ -0,0 +1,32 @@
+import { assets } from "./CoinInput";
+import styled from "styled-components";
+import { Window, Bar, WindowContent } from "react95";
+import React from "react";
+import "98.css";
+
+const Main = styled.div`
+	display: flex;
+	width: 50%;
+	height: 100%;
+	background-color: coral;
+
+	.bar {
+		width: 100%;
+		height: 3rem;
+	}
+`;
+
+const AssetList = () => {
+	return (
+		<Main>
+			<Window>
+				<Bar className="bar">ASSETS</Bar>
+				<WindowContent>
+					<h1>placeholder</h1>
+				</WindowContent>
+			</Window>
+		</Main>
+	);
+};
+
+export default AssetList;
