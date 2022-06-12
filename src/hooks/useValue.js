9280b72a85388c12d350e3246ec0273c9ee77575
nolangh <nolanghiggins@gmail.com>
2022-06-11 19:29:44 -0600

    added custome hook to update total value

---
+++ src/hooks/useValue.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { TotalAssets } from "../comps/CoinInput";
+
+const useValue = () => {
+	const [currentValue, setCount] = useState(0);
+
+	const sum = () =>
+		setCount(
+			TotalAssets.reduce(function(sum, number) {
+				const arraySum = sum + number;
+				return arraySum;
+			}, 0)
+		);
+	return [currentValue, sum];
+};
+
+export { useValue };
