89dc212f0ab1babb4d61045ff9e2e7531b7536f4
nolangh <nolanghiggins@gmail.com>
2022-06-16 17:18:03 -0600

    Added file for reducer function

---
+++ src/context/GlobalContext.js
@@ -0,0 +1,26 @@
+import React, { createContext, useReducer } from "react";
+
+const inititalState = {
+	transactions: [
+		{ id: 1, text: "Flower", amount: -20 },
+		{ id: 2, text: "Salary", amount: 300 },
+		{ id: 3, text: "Book", amount: -10 },
+		{ id: 4, text: "Camera", amount: 150 },
+	],
+};
+
+export const GlobalContext = createContext(inititalState);
+
+export const GlobalProvider = ({ children }) => {
+	const [state, dispatch] = useReducer(AppReducer, inititalState);
+
+	return (
+		<GlobalContext.Provider
+			value={{
+				transactions: state.transactions,
+			}}
+		>
+			{children}
+		</GlobalContext.Provider>
+	);
+};
