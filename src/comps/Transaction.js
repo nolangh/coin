aa92ad91e20104ca017953b14b162ab0c0cdbc99
nolangh <nolanghiggins@gmail.com>
2022-06-16 22:03:21 -0600

    Finished basic logic fro transaction

--- src/comps/Transaction.js
+++ src/comps/Transaction.js
@@ -3,11 +3,15 @@ import React from "react";
 import { Button, Divider, Fieldset } from "react95";
 
 export const Transaction = ({ transaction }) => {
+	const sign = transaction.amount < 0 ? "-" : "+";
+
 	return (
-		<li className="minus">
+		<li className={transaction.amount < 0 ? "minus" : "plus"}>
 			<Fieldset label={transaction.text}>
 				<Button className="deleteButton">X</Button>
-				<span>-{transaction.amount}</span>
+				<span>
+					{sign}${Math.abs(transaction.amount)}
+				</span>
 			</Fieldset>
 			<Divider />
 		</li>
